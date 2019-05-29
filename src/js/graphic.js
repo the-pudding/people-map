c195961e1593741271cbe2df25aab003a035a301
russellgoldenberg <russellgoldenberg@gmail.com>
2019-05-29 14:20:10 -0400

    new data

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -136,7 +136,7 @@ function setupMap() {
 
   map = new mapboxgl.Map({
     container: 'map',
-    style: `mapbox://styles/dock4242/cjv6zwo1e3mdx1fpmeybn7crm`,
+    style: 'mapbox://styles/dock4242/cjw9k00li070n1cp8pfd62kyk',
     center: [-98.57, 39.82],
     zoom: 3,
     maxBounds: [[-180, 0], [-40, 75]],
